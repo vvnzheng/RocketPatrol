2fdbabf412bb2a1ce69a07dc263c770236dcb206
Vivian Zheng <vvnezheng@gmail.com>
2022-04-08 13:48:07 -0700

    Added Score holder and Explosions

--- src/prefabs/Spaceship.js
+++ src/prefabs/Spaceship.js
@@ -15,7 +15,10 @@ class Spaceship extends Phaser.GameObjects.Sprite {
         this.x -= this.moveSpeed;
         //wrap
         if(this.x <= 0 - this.width){
-            this.x = game.config.width;
+            this.reset();
         }
+    }
+    reset(){
+        this.x = game.config.width;
     }
 }
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -7,6 +7,7 @@ class Play extends Phaser.Scene {
         this.load.image('rocket', './assets/rocket.png');
         this.load.image('spaceship', './assets/spaceship.png');
         this.load.image('starfield', './assets/starfield.png');
+        this.load.spritesheet('explosion', './assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
     }
     create(){
         //place tile sprite
@@ -29,6 +30,27 @@ class Play extends Phaser.Scene {
         keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
         keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+        // animation config
+        this.anims.create({
+            key: 'explode',
+            frames: this.anims.generateFrameNumbers('explosion', {start: 0, end: 9, first: 0}),
+            frameRate: 30
+        });
+        this.p1Score = 0;
+        // display score
+        let scoreConfig = {
+            fontFamily: 'Courier',
+            fontSize: '28px',
+            backgroundColor: '#F3B141',
+            color: '#843605',
+            align: 'right',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 100
+        }
+        this.scoreLeft = this.add.text(borderUISize + borderPadding, borderUISize + borderPadding*2, this.p1Score, scoreConfig);
     }
     update(){
         this.starfield.tilePositionX -= 4;
@@ -36,10 +58,9 @@ class Play extends Phaser.Scene {
         this.ship01.update();
         this.ship02.update();
         this.ship03.update();
-        // check collisions
-        if(this.checkCollision(this.p1Rocket, this.ship03) || this.checkCollision(this.p1Rocket, this.ship02) ||this.checkCollision(this.p1Rocket, this.ship01)) {
-            this.p1Rocket.reset();
-        }
+        this.checkCollision(this.p1Rocket, this.ship01);
+        this.checkCollision(this.p1Rocket, this.ship02);
+        this.checkCollision(this.p1Rocket, this.ship03);
     }
     checkCollision(rocket, ship) {
         // simple AABB checking
@@ -47,9 +68,23 @@ class Play extends Phaser.Scene {
             rocket.x + rocket.width > ship.x && 
             rocket.y < ship.y + ship.height &&
             rocket.height + rocket.y > ship. y) {
-                return true;
-        } else {
+                rocket.reset();
+                this.shipExplode(ship);
+                //return true;
+        } /*else {
             return false;
-        }
+        }*/
+    }
+    shipExplode(ship) {
+        // temporarily hide ship
+        ship.alpha = 0;
+        // create explosion sprite at ship's position
+        let boom = this.add.sprite(ship.x, ship.y, 'explosion').setOrigin(0, 0);
+        boom.anims.play('explode');             // play explode animation
+        boom.on('animationcomplete', () => {    // callback after anim completes
+          ship.reset();                         // reset ship position
+          ship.alpha = 1;                       // make ship visible again
+          boom.destroy();                       // remove explosion sprite
+        });       
     }
 }
